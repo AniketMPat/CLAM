2722d777cf7ced228248129e2d5ee0ab8a3e8e1e
AniketMPat <ampatel@ualberta.ca>
2026-02-01 12:42:19 -0700

    change autoformat indent width

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
+#include "CBaseVisitor.h"
 #include "CLexer.h"
 #include "CParser.h"
+#include "CVisitor.h"
 
 // Antlr includes
 #include "ANTLRFileStream.h"
@@ -7,30 +9,31 @@
 #include "tree/ParseTree.h"
 #include "tree/ParseTreeWalker.h"
 
-#include <iostream>
 #include <fstream>
+#include <iostream>
 
 int main(int argc, char **argv) {
-  if (argc < 3) {
-    std::cout << "Missing required argument.\n"
-              << "Required arguments: <input file path> <output file path>\n";
-    return 1;
-  }
+    if (argc < 3) {
+        std::cout
+            << "Missing required argument.\n"
+            << "Required arguments: <input file path> <output file path>\n";
+        return 1;
+    }
 
-  // Open the file then parse and lex it.
-  antlr4::ANTLRFileStream afs;
-  afs.loadFromFile(argv[1]);
-  ClamParser::CLexer lexer(&afs);
-  antlr4::CommonTokenStream tokens(&lexer);
-  ClamParser::CParser parser(&tokens);
+    // Open the file then parse and lex it.
+    antlr4::ANTLRFileStream afs;
+    afs.loadFromFile(argv[1]);
+    ClamParser::CLexer lexer(&afs);
+    antlr4::CommonTokenStream tokens(&lexer);
+    ClamParser::CParser parser(&tokens);
 
-  // Get the root of the parse tree. Use your base rule name.
-  antlr4::tree::ParseTree *tree = parser.file();
+    // Get the root of the parse tree. Use your base rule name.
+    antlr4::tree::ParseTree *tree = parser.file();
 
-  // MyVisitor visitor;
-  // visitor.visit(tree);
+    // MyVisitor visitor;
+    // visitor.visit(tree);
 
-  std::ofstream os(argv[2]);
+    std::ofstream os(argv[2]);
 
-  return 0;
+    return 0;
 }
